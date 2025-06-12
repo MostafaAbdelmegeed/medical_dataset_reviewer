14da6d510ca98a62e4ac452ac8b2bf77d994934f
Mostafa Abdelmegeed <mostafa0as3d@gmail.com>
2025-06-12 12:50:59 +0300

    Add discard button and improve navigation

--- seg_qc_tool/gui.py
+++ seg_qc_tool/gui.py
@@ -14,17 +14,34 @@ from .models import Pair
 
 
 class ImageView(QtWidgets.QLabel):
+    """Widget that displays a grayscale slice scaled to fit."""
+
     def __init__(self) -> None:
         super().__init__()
         self.setAlignment(QtCore.Qt.AlignCenter)
+        self._pixmap = QtGui.QPixmap()
 
     def set_image(self, array) -> None:
+        """Set and scale an image from a numpy array."""
         h, w = array.shape
         img = QtGui.QImage(
             array.tobytes(), w, h, QtGui.QImage.Format.Format_Grayscale8
         )
-        pix = QtGui.QPixmap.fromImage(img)
-        self.setPixmap(pix)
+        self._pixmap = QtGui.QPixmap.fromImage(img)
+        self._update_pixmap()
+
+    def resizeEvent(self, event: QtGui.QResizeEvent) -> None:  # pragma: no cover - GUI
+        self._update_pixmap()
+        super().resizeEvent(event)
+
+    def _update_pixmap(self) -> None:
+        if not self._pixmap.isNull():
+            scaled = self._pixmap.scaled(
+                self.size(),
+                QtCore.Qt.AspectRatioMode.KeepAspectRatio,
+                QtCore.Qt.TransformationMode.SmoothTransformation,
+            )
+            self.setPixmap(scaled)
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -57,11 +74,22 @@ class MainWindow(QtWidgets.QMainWindow):
         self.addToolBar(QtCore.Qt.ToolBarArea.BottomToolBarArea, nav)
 
         prev_act = QtGui.QAction("Prev", self)
+        prev_act.setShortcut(QtGui.QKeySequence(QtCore.Qt.Key_Left))
         prev_act.triggered.connect(self.controller.prev_pair)
         nav.addAction(prev_act)
+
         next_act = QtGui.QAction("Next", self)
+        next_act.setShortcut(QtGui.QKeySequence(QtCore.Qt.Key_Right))
         next_act.triggered.connect(self.controller.next_pair)
         nav.addAction(next_act)
+
+        discard_btn = QtWidgets.QPushButton("Discard")
+        discard_btn.setStyleSheet("background-color: red; color: white;")
+        discard_btn.clicked.connect(self.discard)
+        QtGui.QShortcut(QtGui.QKeySequence(QtCore.Qt.Key_D), self).activated.connect(self.discard)
+        nav.addWidget(discard_btn)
+
+        nav.addSeparator()
 
         self.slice_slider = QtWidgets.QSlider(QtCore.Qt.Orientation.Horizontal)
         nav.addWidget(self.slice_slider)
@@ -99,6 +127,14 @@ class MainWindow(QtWidgets.QMainWindow):
         volume = future.result()
         norm, _, _ = normalize_volume(volume)
         return norm
+
+    # Actions -------------------------------------------------
+    def discard(self) -> None:
+        text, ok = QtWidgets.QInputDialog.getText(
+            self, "Discard Comment", "Comment:", QtWidgets.QLineEdit.Normal, ""
+        )
+        if ok:
+            self.controller.discard_current(text)
 
     # Folder selection ---------------------------------------
     def choose_originals(self) -> None:
